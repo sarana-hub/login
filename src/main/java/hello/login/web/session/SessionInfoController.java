b60cc607c306d8e002e5dabb326b6f2291149d90
nayeon-jo <nyc0516@naver.com>
2022-08-18 20:20:52 +0900

    세션 정보와 타임아웃 설정

--- src/main/java/hello/login/web/session/SessionInfoController.java
+++ src/main/java/hello/login/web/session/SessionInfoController.java
@@ -11,6 +11,7 @@ import java.util.Date;
 @Slf4j
 @RestController
 public class SessionInfoController {
+
     @GetMapping("/session-info")
     public String sessionInfo(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
@@ -22,11 +23,17 @@ public class SessionInfoController {
         session.getAttributeNames().asIterator()
                 .forEachRemaining(name -> log.info("session name={}, value={}", name, session.getAttribute(name)));
 
-        log.info("sessionId={}", session.getId());
-        log.info("maxInactiveInterval={}", session.getMaxInactiveInterval());
-        log.info("creationTime={}", new Date(session.getCreationTime()));
+        log.info("sessionId={}", session.getId());      //세션Id, JSESSIONID 의 값
+        log.info("maxInactiveInterval={}", session.getMaxInactiveInterval());   //세션의 유효 시간
+        log.info("creationTime={}", new Date(session.getCreationTime()));   //세션 생성일시
+
         log.info("lastAccessedTime={}", new Date(session.getLastAccessedTime()));
+        // 세션과 연결된 사용자가 최근에 서버에 접근한 시간
+        // 클라이언트에서 서버로 sessionId( JSESSIONID )를 요청한 경우에 갱신된다
+
         log.info("isNew={}", session.isNew());
+        //새로 생성된 세션인지,
+        //아니면 이미 과거에 만들어졌고, 클라이언트에서 서버로 sessionId( JSESSIONID )를 요청해서 조회된 세션인지, 여부
 
         return "세션 출력";
     }
